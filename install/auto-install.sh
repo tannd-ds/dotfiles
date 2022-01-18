64ffc4bdfed16123b84bc20531e92813095e12ce
tannd-ds <donhattan1012dev@gmail.com>
2022-01-18 23:57:14 +0700

    auto-install.sh - make vim PlugInstall terminal auto close

--- install/auto-install.sh
+++ install/auto-install.sh
@@ -27,7 +27,7 @@ sudo snap install neovim --classic
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # Install Vim Plugins using Vim-plug
-gnome-terminal -- nvim -c ':PlugInstall'
+gnome-terminal -- nvim -c ':PlugInstall|q|q'
 # MS Teams
 echo -e "\033[0;32mInstalling MS Teams...\033[0;37m"
 sudo apt install teams
