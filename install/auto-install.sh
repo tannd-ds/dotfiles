76c5fda5f8bd7c4ac9b8de0e517efec7c78742e6
tannd-ds <donhattan1012dev@gmail.com>
2022-01-22 10:57:23 +0700

    auto-install.sh - fix #3

--- install/auto-install.sh
+++ install/auto-install.sh
@@ -8,6 +8,8 @@ sudo apt install gnome-shell-extensions
 # wget && snap
 echo -e "\033[0;32mInstalling wget...\033[0;37m"
 sudo apt-get install wget
+echo -e "\033[0;32mInstalling curl...\033[0;37m"
+sudo apt install curl
 echo -e "\033[0;32mInstalling snap...\033[0;37m"
 sudo apt install snap
 echo -e "\033[0;32mInstalling git...\033[0;37m"
@@ -24,35 +26,42 @@ rm nodesource_setup.sh
 # Alacrity - Terminal Emulator
 echo -e "\033[0;32mInstalling Alacritty...\033[0;37m"
 sudo snap install alacritty --classic
+
 # neoVim
 echo -e "\033[0;32mInstalling Vim & NeoVim...\033[0;37m"
 sudo apt install vim
 sudo snap install nvim --classic
+
 echo -e "\033[0;32mInstalling tmux...\033[0;37m"
 sudo apt install tmux
-# Install plug.vim for NeoVim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-# Install Vim Plugins using Vim-plug
-gnome-terminal -- nvim -c ':PlugInstall|q|q'
+
 # Python
 echo -e "\033[0;32mInstalling Python...\033[0;37m"
 sudo apt-get install python3 python3-venv python3-pip pip
 sudo pip install jupyter
+
 # MS Teams
 echo -e "\033[0;32mInstalling MS Teams...\033[0;37m"
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
 sudo apt update
 sudo apt install teams
 
 ## ----- MAKE SYMBOLIC LINK FOR CONFIGURATION FILES -----
 echo -e "\033[0;32mCreating Symbolic links for Config files...\033[0;37m"
 # in ~/ folder
+rm ~/.bashrc
 for file in ~/dotfiles/home/.*; do
 	ln -s ${file} ~/
 done
 # in ~/.config folder
 ln -s ~/dotfiles/.config/* ~/.config/
+
+# Install plug.vim for NeoVim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Install Vim Plugins using Vim-plug
+gnome-terminal -- nvim -c ':PlugInstall|q|q'
 
 cd ~/dotfiles/install
 mkdir removeLater
